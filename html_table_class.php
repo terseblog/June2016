9cc4d55664212e65a6ed666ae148ad5659da5982
unknown <apriest@SAT-DUL-AARONP.internal.saturnsys.com>
2016-01-29 16:28:24 -0600

    Adding files

---
+++ html_table_class.php
@@ -0,0 +1,83 @@
+<?php
+//class html_table
+
+class html_table
+{
+  public static $warning_row_values="You have entered more items than expected.";
+  public static $warning_row_values2="You have fewer items than expected.";
+  public $col_names=["Empty"]; //1D numeric arry of names of the columns set by constructor
+  public $col_data; //2D array of values for each column
+  public $col_name_map; //allows displayed column names to be differnt while the keys for col_names[n][] stay the same
+  //this will help when databases are implemented
+  public $css_th_class="tableHeader"; //HTML/CSS class name for column names (headers)
+  public $css_table_class="tableClass"; //HTML/CSS class name for the table
+  public $css_td_class="tableCell"; //HTML/CSS class name for cells
+
+  public function __construct($col_names_from_user, $row_data_from_user)
+  {
+
+    //initalize $col_names
+    $this->col_names = $col_names_from_user;
+
+    //inialize $col_name_map
+    for($i=0; $i<sizeof($this->col_names); $i++){
+        $this->col_name_map[$this->col_names[$i]]=$this->col_names[$i];
+      }
+
+      //initialize $col_data figure this out!!!
+
+      for($i=0; $i<sizeof($row_data_from_user); $i++){
+        for($j=0; $j<sizeof($this->col_names); $j++){
+            $this->col_data[$i][$this->col_names[$j]]=$row_data_from_user[$i][$this->col_names[$j]];
+          }
+        }
+  }
+
+  public function add_row_data($new_row_values)//new_row_values is the associatve array created by entering data or a mysqli result set
+  {
+      for($i=0; $i<sizeof($this->col_names); $i++)
+      {
+        $this->col_data[$i][$this->col_names[$i]]=$new_row_values[$i][$this->col_names[$i]];
+      }
+
+  }
+
+  public function create_html_table()
+  {
+  //create the table html
+  //need to add an array of CSS values
+    echo "<table border=1>";//remove border=1 when debug is over
+
+    //lay down the header row
+    echo "<tr>";
+    //header names are stored here; display them
+   for($i=0; $i<sizeof($this->col_names); $i++)
+   {
+
+     echo "<th>".$this->col_name_map[$this->col_names[$i]]."</th>";
+   }
+   //done with the header row
+   echo "</tr>";
+   //now will add rows to the table the header names are keys to the second dimension of $row_values_arr
+   $this->add_rows_to_table();
+   //close this table
+   echo "</table>";
+  }
+
+  public function add_rows_to_table(){
+  /*need to add IDs to TDs so that odd and even rows can be separate*/
+  //For each numerical index in $row_values_arr[$i]...
+  for($i=0; $i<sizeof($this->col_data); $i++){
+    $row_num=$i+1;//for adding row id's, but I did not want to start with 0
+    echo "<tr id=\"row{$row_num}\">";//Oh, BTW we need a new row for each numerical index in $row_values_arr since each one represents a row
+        //...access the associative key in the $row_values_arr using the names in $header_names_arr as the keys
+        for($j=0; $j<sizeof($this->col_names); $j++){
+          $cell_num=$j+1; //for making an id for each cell, but I did not want to start with 0
+          echo "<td id=\"row{$row_num}cell{$cell_num}\">".$this->col_data[$i][$this->col_names[$j]]."</td>";
+        }
+    echo "</tr>";//end the row after each iteration of this outer for loop
+    }
+
+  }
+}
+?>
---
+++ table_display2.php
@@ -0,0 +1,30 @@
+<?php
+//real implementation of html_table class
+include "head.html";
+include "html_factory.php";
+include "html_table_class.php";
+/*NOTE: a section for making static assignments to test the code*/
+$header_values=["Name","Age","Occupation","Location"];
+$row_values [0] =[$header_values[0] => "John Smith",
+                   $header_values[1] => "27",
+                   $header_values[2] => "JS Developer",
+                   $header_values[3] => "Denver, CO"];
+$row_values [1] =[$header_values[0] => "Jane Winch",
+                  $header_values[1] => "54",
+                  $header_values[2] => "Secretary",
+                  $header_values[3] => "Denver, CO"];
+$row_values [2] =[$header_values[0] => "Bill Waulk",
+                   $header_values[1] => "47",
+                   $header_values[2] => "DBA",
+                   $header_values[3] => "Colorado Springs, CO"];
+$row_values [3] =[$header_values[0] => "Nahseem Joulad",
+                  $header_values[1] => "32",
+                  $header_values[2] => "UI Developer",
+                  $header_values[3] => "Boulder, CO"];
+$html_table_obj= new html_table($header_values, $row_values);
+//Some test code
+
+//test code
+//add_row_data($new_row_values)
+$html_table_obj->create_html_table();
+?>
