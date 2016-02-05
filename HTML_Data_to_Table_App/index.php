4176d078bba1ea7b310c5e3b015c7fcf51bb3b55
unknown <apriest@SAT-DUL-AARONP.internal.saturnsys.com>
2016-02-05 16:24:20 -0600

    Reworked files

--- HTML_Data_to_Table_App/index.php
+++ HTML_Data_to_Table_App/index.php
@@ -2,5 +2,6 @@
 //A cool app which has one associated class which turns two arrays (one for headers and one for columns into an HTML table)
 include "head.html";
 include "sample_employees.php";
+include "sample_database.php";
 include "foot.html";
 ?>
---
+++ HTML_Data_to_Table_App/sample_database.php
@@ -0,0 +1,4 @@
+<?php
+echo "Nothing, yet.";
+//implement a database call which returns two arrays one for headers and one for column data
+?>
