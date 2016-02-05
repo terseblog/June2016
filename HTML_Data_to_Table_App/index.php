eceb7b47647816f37ad247964147a29f2cf38970
unknown <apriest@SAT-DUL-AARONP.internal.saturnsys.com>
2016-02-05 16:21:09 -0600

    created the full index file

--- HTML_Data_to_Table_App/index.php
+++ HTML_Data_to_Table_App/index.php
@@ -1,4 +1,6 @@
 <?php
 //A cool app which has one associated class which turns two arrays (one for headers and one for columns into an HTML table)
+include "head.html";
 include "sample_employees.php";
+include "foot.html";
 ?>
